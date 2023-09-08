3e610fbf557a5ef495057a22a3340142f4f8ba91
parksangbin <zaza1599@gmail.com>
2023-09-08 18:54:43 +0900

    commit

--- ch2/src/main/java/com/fastcampus/ch2/ExceptionController.java
+++ ch2/src/main/java/com/fastcampus/ch2/ExceptionController.java
@@ -2,47 +2,39 @@ package com.fastcampus.ch2;
 
 import java.io.FileNotFoundException;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class ExceptionController {
-	
-	//아래의 객체들처럼 중복됐을때 사용해주면 좋음
-	// try - catch 대신에 @ExceptionHandler를 써줌
-	@ExceptionHandler(Exception.class)
-	public String catcher(Exception ex, Model m) {
-		m.addAttribute("ex", ex);
-		return "error";
-	}
-	
 	@ExceptionHandler({NullPointerException.class, FileNotFoundException.class})
 	public String catcher2(Exception ex, Model m) {
 		m.addAttribute("ex", ex);
 		return "error";
 	}
-	
+
+	@ExceptionHandler(Exception.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) // 200 -> 500
+	public String catcher(Exception ex, Model m) {
+		System.out.println("catcher() in ExceptionController");
+		System.out.println("m="+m);
+//		m.addAttribute("ex", ex);
+
+		return "error";
+	}
 	
 	@RequestMapping("/ex")
-	public String main() throws Exception {
-		throw new Exception("예외가 발생하였습니다.");
-		}
-	
+	public String main(Model m) throws Exception {
+		m.addAttribute("msg", "message from ExceptionController.main()");
+		throw new Exception("예외발생");
+	}
+
 	@RequestMapping("/ex2")
 	public String main2() throws Exception {
-		throw new FileNotFoundException("예외가 발생하였습니다.");
-		}
-	
-//	@RequestMapping("/ex2")
-//	public String main2() throws Exception {
-//		try {
-//			throw new Exception("예외가 발생하였습니다.");
-//		} catch (Exception e) {
-//	
-//			return "error";
-//		}
-//	}
-	
+		throw new NullPointerException("예외발생");
+	}
 }
